28d5a0b80c522b20472fcf125b75d9830bcadafe
inthebrilliantblue <intheprettypink@gmail.com>
2021-01-06 11:55:33 -0600

    Create cockpit.sh

---
+++ firstsetup/cockpit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Install cockpit and packages
+sudo apt install cockpit cockpit-dashboard cockpit-machines cockpit-networkmanager cockpit-packagekit cockpit-podman cockpit-storaged cockpit-system
+#ZFS beta package
+git clone https://github.com/optimans/cockpit-zfs-manager.git
+sudo cp -r cockpit-zfs-manager/zfs /usr/share/cockpit
+rm -rf cockpit-zfs-manager

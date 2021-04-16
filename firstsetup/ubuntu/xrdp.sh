e706bdb26b15f292a97428a35d3a3d1a891332ba
inthebrilliantblue <intheprettypink@gmail.com>
2021-04-16 08:55:12 -0500

    Create xrdp.sh

---
+++ firstsetup/ubuntu/xrdp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Installing xrdp..."
+sudo apt install xrdp -y
+
+#Now setup the ubuntu color profile rules
+echo "Setting up color profile rules..."
+sudo bash -c "cat >/etc/polkit-1/localauthority/50-local.d/45-allow.colord.pkla" <<EOF
+[Allow Colord all Users]
+Identity=unix-user:*
+Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+EOF
+#End of paste
+
+echo 'Installing XRDP - Done!'

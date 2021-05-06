a96f6bfc44ba4c02441245820a44c0ed10b57113
inthebrilliantblue <intheprettypink@gmail.com>
2021-05-06 09:30:54 -0500

    Add cockpit-pcp for metrics history

--- firstsetup/ubuntu/cockpit.sh
+++ firstsetup/ubuntu/cockpit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Install cockpit and packages
-sudo apt install cockpit cockpit-dashboard cockpit-machines cockpit-networkmanager cockpit-packagekit cockpit-podman cockpit-storaged cockpit-system
+sudo apt install cockpit cockpit-dashboard cockpit-machines cockpit-networkmanager cockpit-packagekit cockpit-podman cockpit-storaged cockpit-system cockpit-pcp
 #ZFS beta package
 git clone https://github.com/optimans/cockpit-zfs-manager.git
 sudo cp -r cockpit-zfs-manager/zfs /usr/share/cockpit

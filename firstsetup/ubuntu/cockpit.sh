d2256b2d17ff01fa52c514e578ec400ae9d97e73
inthebrilliantblue <intheprettypink@gmail.com>
2021-06-22 12:25:52 -0500

    Update cockpit.sh
    Remove cockpit-dashboard.

--- firstsetup/ubuntu/cockpit.sh
+++ firstsetup/ubuntu/cockpit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Install cockpit and packages
-sudo apt install cockpit cockpit-dashboard cockpit-machines cockpit-networkmanager cockpit-packagekit cockpit-podman cockpit-storaged cockpit-system cockpit-pcp
+#cockpit-dashboard no longer a thing in Ubuntu 21.04
+sudo apt install cockpit cockpit-machines cockpit-networkmanager cockpit-packagekit cockpit-podman cockpit-storaged cockpit-system cockpit-pcp cockpit-ws cockpit-bridge
 #ZFS beta package
 git clone https://github.com/optimans/cockpit-zfs-manager.git
 sudo cp -r cockpit-zfs-manager/zfs /usr/share/cockpit

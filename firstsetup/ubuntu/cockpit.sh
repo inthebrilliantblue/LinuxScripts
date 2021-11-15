53e15cad4423ab7a640bf8204600738a7f15bdf8
inthebrilliantblue <intheprettypink@gmail.com>
2021-11-15 08:39:01 -0600

    Update cockpit.sh
    Added git to package install line for systems that may not have git pre-installed.

--- firstsetup/ubuntu/cockpit.sh
+++ firstsetup/ubuntu/cockpit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Install cockpit and packages
 #cockpit-dashboard no longer a thing in Ubuntu 21.04
-sudo apt install cockpit cockpit-machines cockpit-networkmanager cockpit-packagekit cockpit-podman cockpit-storaged cockpit-system cockpit-pcp cockpit-ws cockpit-bridge
+sudo apt install git cockpit cockpit-machines cockpit-networkmanager cockpit-packagekit cockpit-podman cockpit-storaged cockpit-system cockpit-pcp cockpit-ws cockpit-bridge
 #ZFS beta package
 git clone https://github.com/optimans/cockpit-zfs-manager.git
 sudo cp -r cockpit-zfs-manager/zfs /usr/share/cockpit

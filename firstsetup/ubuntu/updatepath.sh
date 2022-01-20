1ba28e124e2e17830b0b37e3dee7813cfb92d60f
inthebrilliantblue <intheprettypink@gmail.com>
2022-01-20 14:43:19 -0600

    Create updatepath.sh

---
+++ firstsetup/ubuntu/updatepath.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Create the custom bin folder
+sudo mkdir /cusbin
+
+#Update system wide PATH to include custom scripts and bins
+sudo echo "" >> /etc/profile
+sudo echo "#Add custom scripts and bins" >> /etc/profile
+sudo echo "export PATH=$PATH:/cusbin" >> /etc/profile
+
+#Copy github scripts to /scripts
+cd /cusbin
+sudo wget https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/listzfs.sh
+sudo wget https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/sshkeygen.sh
+sudo wget https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/count.sh
+
+#Set permissions
+sudo chmod -R 755 /cusbin

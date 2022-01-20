159f2b27a49215f24c25cd40d983e682fbb12ef0
inthebrilliantblue <intheprettypink@gmail.com>
2022-01-20 14:44:52 -0600

    Update updatepath.sh

--- firstsetup/ubuntu/updatepath.sh
+++ firstsetup/ubuntu/updatepath.sh
@@ -8,7 +8,7 @@ sudo echo "" >> /etc/profile
 sudo echo "#Add custom scripts and bins" >> /etc/profile
 sudo echo "export PATH=$PATH:/cusbin" >> /etc/profile
 
-#Copy github scripts to /scripts
+#Copy github scripts to /cusbin
 cd /cusbin
 sudo wget https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/listzfs.sh
 sudo wget https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/sshkeygen.sh

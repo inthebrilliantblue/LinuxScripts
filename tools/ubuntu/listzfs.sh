3b260a55fbd9ea1ec493c9976b1866b3cb0fa0e9
inthebrilliantblue <intheprettypink@gmail.com>
2022-02-15 09:07:42 -0600

    Update listzfs.sh

--- tools/ubuntu/listzfs.sh
+++ tools/ubuntu/listzfs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #List datasets
-printf "ZFS Datasets"
+printf "ZFS Datasets\n"
 zfs list -r -t filesystem -o name,used,refer,avail,recordsize,compressratio,compression,sync,mountpoint,primarycache,secondarycache,dedup
 printf "\n"
 
 #List ZVOLs
-printf "ZFS ZVOLs"
+printf "ZFS ZVOLs\n"
 zfs list -r -t volume -o name,used,refer,avail,volsize,volblocksize,compressratio,compression,sync,primarycache,secondarycache,dedup
 printf "\n"
 
 #List pools
-printf "ZPOOLs"
+printf "ZPOOLs\n"
 zpool list

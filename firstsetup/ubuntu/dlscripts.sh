c8678281793db671d76e4f7bf9cf31e8b1b10c1d
inthebrilliantblue <intheprettypink@gmail.com>
2022-02-15 09:05:01 -0600

    Update dlscripts.sh

--- firstsetup/ubuntu/dlscripts.sh
+++ firstsetup/ubuntu/dlscripts.sh
@@ -5,13 +5,17 @@ DL="/usr/local/bin"
 
 #Copy github scripts to $DL
 cd "${DL}"
+rm "${DL}/listzfs.sh"
 sudo wget -O "${DL}/listzfs.sh" https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/listzfs.sh
+rm "${DL}/sshkeygen.sh"
 sudo wget -O "${DL}/sshkeygen.sh" https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/sshkeygen.sh
+rm "${DL}/count.sh"
 sudo wget -O "${DL}/count.sh" https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/count.sh
 
 #Set permissions
 sudo chmod -R +x "${DL}"
 
 #Download self to update
+rm "${DL}/dlscripts.sh"
 sudo wget -O "${DL}/dlscripts.sh" https://github.com/inthebrilliantblue/LinuxScripts/raw/main/firstsetup/ubuntu/dlscripts.sh
 sudo chmod +x "${DL}/dlscripts.sh"

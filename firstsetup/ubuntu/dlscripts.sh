7eb3c4d783e90bc7c747f52236fa8417f259835d
inthebrilliantblue <intheprettypink@gmail.com>
2022-02-15 10:41:45 -0600

    Update dlscripts.sh

--- firstsetup/ubuntu/dlscripts.sh
+++ firstsetup/ubuntu/dlscripts.sh
@@ -6,16 +6,16 @@ DL="/usr/local/bin"
 #Copy github scripts to $DL
 cd "${DL}"
 rm "${DL}/listzfs.sh"
-sudo wget -O "${DL}/listzfs.sh" https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/listzfs.sh
+sudo wget --no-check-certificate --no-cache --no-cookies -O "${DL}/listzfs.sh" https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/listzfs.sh
 rm "${DL}/sshkeygen.sh"
-sudo wget -O "${DL}/sshkeygen.sh" https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/sshkeygen.sh
+sudo wget --no-check-certificate --no-cache --no-cookies -O "${DL}/sshkeygen.sh" https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/sshkeygen.sh
 rm "${DL}/count.sh"
-sudo wget -O "${DL}/count.sh" https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/count.sh
+sudo wget --no-check-certificate --no-cache --no-cookies -O "${DL}/count.sh" https://raw.githubusercontent.com/inthebrilliantblue/LinuxScripts/main/tools/ubuntu/count.sh
 
 #Set permissions
 sudo chmod -R +x "${DL}"
 
 #Download self to update
 rm "${DL}/dlscripts.sh"
-sudo wget -O "${DL}/dlscripts.sh" https://github.com/inthebrilliantblue/LinuxScripts/raw/main/firstsetup/ubuntu/dlscripts.sh
+sudo wget --no-check-certificate --no-cache --no-cookies -O "${DL}/dlscripts.sh" https://github.com/inthebrilliantblue/LinuxScripts/raw/main/firstsetup/ubuntu/dlscripts.sh
 sudo chmod +x "${DL}/dlscripts.sh"

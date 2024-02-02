ec6dccdf40bc0fbc21865e85a7834ef912f4517a
inthebrilliantblue <intheprettypink@gmail.com>
2024-02-02 08:07:47 -0600

    Update listzfs.sh
    Add ability to filter by dataset on zfs list.

--- tools/ubuntu/listzfs.sh
+++ tools/ubuntu/listzfs.sh
@@ -2,17 +2,17 @@
 
 #List datasets
 printf "ZFS Datasets\n"
-zfs list -r -t filesystem -o name,used,refer,avail,recordsize,compressratio,compression,sync,mountpoint,primarycache,secondarycache,dedup,sharenfs
+zfs list -r -t filesystem -o name,used,refer,avail,recordsize,compressratio,compression,sync,mountpoint,primarycache,secondarycache,dedup,sharenfs $1
 printf "\n"
 
 #List encrypted dataset values
 printf "ZFS Encrypted Datasets\n"
-zfs list -r -t filesystem -o name,keystatus,keylocation,keyformat,encryption,mounted | grep -v " - "
+zfs list -r -t filesystem -o name,keystatus,keylocation,keyformat,encryption,mounted $1 | grep -v " - "
 printf "\n"
 
 #List ZVOLs
 printf "ZFS ZVOLs\n"
-zfs list -r -t volume -o name,used,refer,avail,volsize,volblocksize,compressratio,compression,logbias,sync,primarycache,secondarycache,dedup
+zfs list -r -t volume -o name,used,refer,avail,volsize,volblocksize,compressratio,compression,logbias,sync,primarycache,secondarycache,dedup $1
 printf "\n"
 
 #List pools

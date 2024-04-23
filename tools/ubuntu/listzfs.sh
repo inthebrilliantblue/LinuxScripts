9de527e821b9f822149f9381565c8edb967c4840
inthebrilliantblue <intheprettypink@gmail.com>
2024-04-23 15:30:01 -0500

    Update listzfs.sh
    Add ability to list snapshot counts of zfs dataset recursively with --snapshotcounts / -sc, and added ability to specify zfs dataset.

--- tools/ubuntu/listzfs.sh
+++ tools/ubuntu/listzfs.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-#List datasets
-printf "ZFS Datasets\n"
-zfs list -r -t filesystem -o name,used,refer,avail,recordsize,compressratio,compression,sync,mountpoint,primarycache,secondarycache,dedup,sharenfs $1
-printf "\n"
+#Get options
+while [ True ]; do
+	if [ "$1" = "--dataset" -o "$1" = "-d" ]; then
+		DATASET="$2"
+		shift 2
+	elif [ "$1" = "--snapshotcounts" -o "$1" = "-sc" ]; then
+		SNAPSHOTCOUNTS=1
+		shift 1
+	else
+		DATASET="$1"
+		break
+	fi
+done
 
-#List encrypted dataset values
-printf "ZFS Encrypted Datasets\n"
-zfs list -r -t filesystem -o name,keystatus,keylocation,keyformat,encryption,mounted $1 | grep -v " - "
-printf "\n"
+#Determine what to display
+if [ $SNAPSHOTCOUNTS ]; then
+	#Display counts of snapshots
+	zfs list  -t snapshot -o name -s name -r -H $DATASET | sed 's/@.*//' | uniq -c | head
+else
+	#Show default data
+	#List datasets
+	printf "ZFS Datasets\n"
+	zfs list -r -t filesystem -o name,used,refer,avail,recordsize,compressratio,compression,sync,mountpoint,primarycache,secondarycache,dedup,sharenfs $DATASET
+	printf "\n"
 
-#List ZVOLs
-printf "ZFS ZVOLs\n"
-zfs list -r -t volume -o name,used,refer,avail,volsize,volblocksize,compressratio,compression,logbias,sync,primarycache,secondarycache,dedup $1
-printf "\n"
+	#List encrypted dataset values
+	printf "ZFS Encrypted Datasets\n"
+	zfs list -r -t filesystem -o name,keystatus,keylocation,keyformat,encryption,mounted $DATASET | grep -v " - "
+	printf "\n"
 
-#List pools
-printf "ZPOOLs\n"
-zpool list
+	#List ZVOLs
+	printf "ZFS ZVOLs\n"
+	zfs list -r -t volume -o name,used,refer,avail,volsize,volblocksize,compressratio,compression,logbias,sync,primarycache,secondarycache,dedup $DATASET
+	printf "\n"
+
+	#List pools
+	printf "ZPOOLs\n"
+	zpool list
+fi

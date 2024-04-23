27633a62dd2eda2c52b6befff6a36512b2824e0f
inthebrilliantblue <intheprettypink@gmail.com>
2024-04-23 15:35:07 -0500

    Update convert-ziptocbz.sh
    Add ability to specify directory to work on.

--- tools/ubuntu/convert-ziptocbz.sh
+++ tools/ubuntu/convert-ziptocbz.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-
-DIR="$(pwd)"
-
-for i in "$DIR"/*.zip; do
+#First determine location
+if [ "$1" != "" ]; then
+        LOC="$1"
+else
+        echo "Using current working location: $PWD"
+        LOC="$PWD"
+fi
+#CD into directory
+cd "$LOC"
+#Now get all .zips and rename them to .cbz
+for i in "$LOC"/*.zip; do
         OLDNAME=`basename "$i"`
         echo "$OLDNAME"
         NAME=`basename "$i" ".zip"`

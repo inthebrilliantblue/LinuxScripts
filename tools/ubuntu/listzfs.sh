16843359d3c34e8f64c4e283946d89daaa50fbaf
inthebrilliantblue <intheprettypink@gmail.com>
2025-05-16 07:43:20 -0500

    Update listzfs.sh
    Feature improvements and bug fixes.

--- tools/ubuntu/listzfs.sh
+++ tools/ubuntu/listzfs.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 #Get options
-while [ True ]; do
+while [ $# -gt 0 ]; do
 	if [ "$1" = "--dataset" -o "$1" = "-d" ]; then
 		DATASET="$2"
 		shift 2
 	elif [ "$1" = "--snapshotcounts" -o "$1" = "-sc" ]; then
 		SNAPSHOTCOUNTS=1
 		shift 1
+	elif [ "$1" = "--more" -o "$1" = "-m" ]; then
+		MORE=1
+		shift 1
+	elif [ "$1" = "--help" -o "$1" = "-h" ]; then
+		printf "listzfs.sh [ARGUMENTS]\n"
+		printf "\t--dataset -d\t\tSpecify dataset to show information about.\n"
+		printf "\t--snapshotcounts -sc\tShow a count of snapshots per dataset.\n"
+		printf "\t--more -m\t\tShow more information about datasets.\n"
+		printf "\t--help -h\t\tShow this help text.\n"
+		exit
 	else
-		DATASET="$1"
-		break
+		printf "\tUnknown argument: $1\n\n"
 	fi
 done
 
@@ -19,15 +28,16 @@ if [ $SNAPSHOTCOUNTS ]; then
 	#Display counts of snapshots
 	zfs list  -t snapshot -o name -s name -r -H $DATASET | sed 's/@.*//' | uniq -c | head
 else
-	#Show default data
- 	#List reservations and quotas
-        printf "ZFS Dataset Info\n"
-        zfs list -r -t filesystem -o name,sync,mountpoint,primarycache,secondarycache,dedup,sharenfs $DATASET
-        printf "\n"
+	#Show more information about dataset
+	if [ $MORE ]; then
+	        printf "ZFS Dataset Info\n"
+	        zfs list -r -t filesystem -o name,sync,primarycache,secondarycache,sharenfs $DATASET
+	        printf "\n"
+	fi
 	
-	#List datasets
+	#Show default data
 	printf "ZFS Dataset Storage Info\n"
-	zfs list -r -t filesystem -o name,quota,reservation,used,refer,avail,recordsize,compressratio,compression $DATASET
+	zfs list -r -t filesystem -o name,mountpoint,quota,reservation,used,refer,avail,recordsize,compressratio,compression $DATASET
 	printf "\n"
 
 	#List encrypted dataset values
@@ -37,7 +47,15 @@ else
 
 	#List ZVOLs
 	printf "ZFS ZVOLs\n"
-	zfs list -r -t volume -o name,used,refer,avail,volsize,volblocksize,compressratio,compression,logbias,sync,primarycache,secondarycache,dedup $DATASET
+	zfs list -r -t volume -o name,used,refer,avail,volsize,volblocksize,compressratio,compression,logbias,sync,primarycache,secondarycache $DATASET
+	printf "\n"
+
+	#Show dedup status
+	printf "Dedup Status\n"
+	for i in $(zpool list -H -o name); do
+		dedupstatus=$(zfs get dedup -H $i | cut -f 3)
+		printf "$dedupstatus\t$i\n"
+	done
 	printf "\n"
 
 	#List pools
